b0c67685d9e49e3a3793dea55083c7bc7a5bc374
Shreeya Chand <sachand@usc.edu>
2026-07-11 21:39:11 -0400

    changed to date tyep

--- backend/pb_migrations/1783815805_updated__audits.js
+++ backend/pb_migrations/1783815805_updated__audits.js
@@ -4,19 +4,16 @@ migrate((app) => {
 
   // add field
   collection.fields.addAt(7, new Field({
-    "autogeneratePattern": "",
     "help": "",
     "hidden": false,
-    "id": "text_created_at",
-    "max": 50,
-    "min": 0,
+    "id": "date_created_at",
+    "max": "",
+    "min": "",
     "name": "created_at",
-    "pattern": "",
     "presentable": false,
-    "primaryKey": false,
     "required": false,
     "system": false,
-    "type": "text"
+    "type": "date"
   }))
 
   return app.save(collection)
@@ -24,7 +21,7 @@ migrate((app) => {
   const collection = app.findCollectionByNameOrId("pbc_1512408581")
 
   // remove field
-  collection.fields.removeById("text_created_at")
+  collection.fields.removeById("date_created_at")
 
   return app.save(collection)
 })
